805b1c5aace1fae546bbdd668d8c89673290f990
ahmet <mr.muhammetmyradow@yandex.ru>
2016-05-22 13:57:51 +0300

    create Calculate

---
+++ Calculate.java
@@ -0,0 +1,9 @@
+public class Calculate{
+	public static void main(String[] args) {
+		System.out.println("Ahmet");
+		/*int first = Integer.indexOf(args[0]);
+		int second = Integer.indexOf(args[1]);
+		int summa = first + second;
+		Syste.out.println("Summa: " + summa);*/
+	}
+}
